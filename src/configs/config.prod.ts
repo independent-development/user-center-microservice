2d66729964278e162de4dfa96599da6a66996c60
yushicheng <1542874601@qq.com>
2022-07-07 01:43:05 +0800

    temp:临时节点标记 星期四 凌晨 1:43:05 七月 7日 2022 修改了 src/configs/config.prod.ts

--- src/configs/config.prod.ts
+++ src/configs/config.prod.ts
@@ -4,10 +4,9 @@ import * as redisStore from "cache-manager-redis-store";
 export const jwt_secret = "local_jwt_secret";
 export const cache_module_config = {
   store: redisStore,
-  host: "redis-prod.jnmb39.clustercfg.apse1.cache.amazonaws.com",
+  host: "redis-prod-mian-001.jnmb39.0001.apse1.cache.amazonaws.com",
   port: 6379,
   ttl: 60 * 60 * 1000,
-  db: 0,
 };
 export const mysql_module_config: TypeOrmModuleOptions = {
   port: 3306,
